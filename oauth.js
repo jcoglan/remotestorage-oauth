f4a54c2d6900be8de4b94747e5c075cda0aff8a7
James Coglan <jcoglan@gmail.com>
2012-09-22 15:10:14 +0100

    Make it work against 5apps.com using a text-mode browser.

--- oauth.js
+++ oauth.js
@@ -1,14 +1,27 @@
 var child = require('child_process'),
     fs    = require('fs'),
     http  = require('http'),
-    qs    = require('querystring');
+    qs    = require('querystring'),
+    url   = require('url'),
+    Proxy = require('./proxy');
+
+var extractToken = function(query, callback) {
+  var params = qs.parse(query),
+      token  = params.access_token,
+      error  = params.hasOwnProperty('error') ? new Error(params.error_description) : null;
+  
+  if (error) error.type = params.error;
+  callback(error, token);
+};
 
 var authenticate = function(target, clientId, scopes, callback) {
-  var server = http.createServer(function(request, response) {
-    var body = '';
-    request.setEncoding('utf-8');
-    request.addListener('data', function(c) { body += c });
-    request.addListener('end', function() {
+  var uri    = url.parse(target, true),
+      inline = (process.env.INLINE !== undefined),
+      browser,
+      server;
+  
+  var receiver = http.createServer(function(request, response) {
+    Proxy.buffer(request, function() {
       if (request.url !== '/') {
         response.writeHead(404, {});
         return response.end();
@@ -16,43 +29,57 @@ var authenticate = function(target, clientId, scopes, callback) {
       
       if (request.method === 'GET') {
         fs.readFile(__dirname + '/receive.html', function(error, content) {
-          response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-          response.write(content, 'utf-8');
-          response.end();
+          response.writeHead(200, {'Content-Type': 'text/html'});
+          response.end(content, 'utf-8');
         });
       }
       else if (request.method === 'POST') {
         response.writeHead(200, {});
         response.end();
-        server.close();
-        
-        var params = qs.parse(body),
-            token  = params.access_token,
-            error  = params.hasOwnProperty('error') ? new Error(params.error_description) : null;
-        
-        if (error) error.type = params.error;
-        callback(error, token);
+        receiver.close();
+        extractToken(request.body, callback);
       }
     });
   });
+  
+  var proxy = Proxy.createPool(function(res) {
+    var hash = url.parse(res.headers.location || '').hash || '';
+    
+    if (res.statusCode < 301 || res.statusCode > 303 || !/\baccess_token=/.test(hash))
+      return false;
+    
+    browser.kill();
+    proxy.close();
+    extractToken(hash.substr(1), callback);
+    return true;
+  });
+  
+  server = inline ? proxy.forOrigin(uri.protocol + '//' + uri.host) : receiver;
   
   server.listen(0, function() {
     var addr   = server.address(),
         client = 'http://' + addr.address + ':' + addr.port + '/',
         params = {client_id: clientId, redirect_uri: client, response_type: 'token', scope: scopes.join(' ')};
     
-    target += '?' + qs.stringify(params);
+    var providerUrl = inline
+                    ? 'http://' + addr.address + ':' + addr.port + uri.pathname + (uri.search || '')
+                    : target;
     
-    var cmds = {win32: 'cmd', darwin: 'open', other: 'xdg-open'},
-        cmd  = cmds[process.platform] || cmds.other,
-        args = (process.platform === 'win32') ? ['/c', 'start', '""', target] : [target];
+    providerUrl += '?' + qs.stringify(params);
     
-    child.execFile(cmd, args);
+    var cmds    = {win32: 'cmd', darwin: 'open', other: 'xdg-open'},
+        command = process.env.CMD || cmds[process.platform] || cmds.other,
+        args    = (command === 'cmd') ? ['/c', 'start', '""', providerUrl] : [providerUrl],
+        options = {};
+    
+    if (inline) options.stdio = [0,2,2];
+    browser = child.spawn(command, args, options);
   });
 };
 
 // e.g.
-authenticate('https://local.dev/oauth/me', 'vault', ['vault:rw'], function(error, token) {
-  console.log(token);
+authenticate('https://5apps.com/rs/oauth/jcoglan', 'vault', ['vault:rw'], function(error, token) {
+  console.log('TOKEN', token);
   process.exit();
 });
+
---
+++ proxy.js
@@ -0,0 +1,101 @@
+var http  = require('http'),
+    https = require('https'),
+    url   = require('url');
+
+var Proxy = function(pool, origin) {
+  var uri = url.parse(origin);
+  
+  this._server = http.createServer(function(request, response) {
+    var client = (uri.protocol === 'https:') ? https : http,
+        requri = url.parse(request.url);
+    
+    request.headers.host = uri.host;
+    
+    var req = client.request({
+      host:     uri.host,
+      port:     uri.port,
+      method:   request.method,
+      path:     requri.pathname + (requri.search || ''),
+      headers:  request.headers
+    });
+    req.addListener('response', function(res) {
+      Proxy.buffer(res, function() {
+        pool.rewrite(res, function() {
+          if (pool.filter(res)) return;
+          delete res.headers['transfer-encoding'];
+          response.writeHead(res.statusCode, res.headers);
+          response.end(res.body);
+        });
+      });
+    });
+    request.pipe(req);
+  });
+};
+
+['listen', 'close', 'address'].forEach(function(method) {
+  Proxy.prototype[method] = function() {
+    return this._server[method].apply(this._server, arguments);
+  };
+});
+
+var Pool = function(filter) {
+  this._filter  = filter;
+  this._proxies = {};
+};
+
+Pool.prototype.close = function() {
+  for (var origin in this._proxies) this._proxies[origin].close();
+};
+
+Pool.prototype.filter = function(response) {
+  return this._filter(response);
+};
+
+Pool.prototype.forOrigin = function(origin, filter) {
+  this._proxies[origin] = this._proxies[origin] || new Proxy(this, origin);
+  return this._proxies[origin];
+};
+
+Pool.prototype.listen = function(origin, callback) {
+  var proxy = this.forOrigin(origin),
+      addr  = proxy.address();
+  
+  if (addr) return callback('http://' + addr.address + ':' + addr.port);
+  
+  proxy.listen(0, function() {
+    var addr = proxy.address();
+    callback('http://' + addr.address + ':' + addr.port);
+  });
+};
+
+Pool.prototype.rewrite = function(response, callback) {
+  var location = response.headers.location;
+  if (location === undefined) return callback();
+  
+  var uri    = url.parse(location),
+      origin = uri.protocol + '//' + uri.host;
+  
+  if (uri.hostname === '0.0.0.0') return callback();
+  
+  this.listen(origin, function(host) {
+    response.headers.location = host + uri.pathname + (uri.search || '');
+    callback();
+  });
+};
+
+Proxy.buffer = function(stream, callback) {
+  var body = '';
+  stream.setEncoding('utf-8');
+  stream.addListener('data', function(c) { body += c });
+  stream.addListener('end', function() {
+    stream.body = body;
+    callback();
+  });
+};
+
+Proxy.createPool = function(filter) {
+  return new Pool(filter);
+};
+
+module.exports = Proxy;
+
