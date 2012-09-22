ffb961928f9ae3a3cdf2866aef3d419c541b613d
James Coglan <jcoglan@gmail.com>
2012-09-22 15:21:03 +0100

    Structure code as a library.

---
+++ example.js
@@ -0,0 +1,12 @@
+var oauth = require('./lib/oauth');
+
+var endpoint = 'https://5apps.com/rs/oauth/jcoglan',
+    client   = 'Vault',
+    scopes   = ['vault:rw'],
+    options  = {browser: process.env.BROWSER, inline: process.env.INLINE};
+    
+oauth.authorize(endpoint, client, scopes, options, function(error, token) {
+  console.log(token);
+  process.exit();
+});
+
